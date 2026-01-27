3e092601cf644c6e595a3a9a04a7bedde55616c8
aftab <aftab.shaikh@ecosmob.com>
2026-01-27 17:46:51 +0530

    type checking switch practice

--- switch.go
+++ switch.go
@@ -24,4 +24,25 @@ func main() {
 	default:
 		fmt.Println("Its working day --", time.Now().Weekday())
 	}
+
+	IntType := func(i interface{}) {
+		// The type switch statement is used to select the type of the interface
+		// value and execute different code depending on the type. The type
+		// switch statement is similar to the regular switch statement, but
+		// it is used to switch on the type of the value rather than the value
+		// itself. The type switch statement is used to inspect the type of
+		// an interface value and execute different code depending on the type.
+		switch t := i.(type) {
+		case int:
+			fmt.Println("Its an interger", t)
+		case string:
+			fmt.Println("Its an string", t)
+		default:
+			fmt.Println("Other type", t)
+		}
+	}
+
+	IntType("aftab")
+	IntType(10)
+
 }
